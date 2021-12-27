8c004a25b1acc4c449dbc962befe97b8e00cfa02
itsjackbd <93772995+itsjackbd@users.noreply.github.com>
2021-12-27 14:51:57 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onclite.mk
+    $(LOCAL_DIR)/ssos_onclite.mk
+
+COMMON_LUNCH_CHOICES := \
+    ssos_onclite-user \
+    ssos_onclite-userdebug \
+    ssos_onclite-eng
